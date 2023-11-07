9f43047acdace8e65d913e58f740b5a2ffcf6c0a
apfwnls14 <apfwnls14@naver.com>
2023-11-07 12:59:29 +0900

    #1.6. 스크롤다음페이지

--- frontend/src/SearchResult.js
+++ frontend/src/SearchResult.js
@@ -3,7 +3,7 @@ class SearchResult {
   data = null;
   onClick = null;
 
-  constructor({ $target, initialData, onClick }) {
+  constructor({ $target, initialData, onClick, onNextPage }) {
     const $wrapper = document.createElement('section');
     this.$searchResult = document.createElement('ul');
     this.$searchResult.className = 'SearchResult';
@@ -12,6 +12,7 @@ class SearchResult {
 
     this.data = initialData;
     this.onClick = onClick;
+    this.onNextPage = onNextPage;
 
     this.render();
   }
@@ -20,6 +21,27 @@ class SearchResult {
     this.data = nextData;
     this.render();
   }
+
+  isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  applyEventToElement = (items) => {
+    document.addEventListener('scroll', () => {
+      items.forEach((el, index) => {
+        if (this.isElementInViewport(el) && items.length - 1 === index) {
+          this.onNextPage();
+        }
+      });
+    });
+  };
 
   render() {
     this.$searchResult.innerHTML = this.data
@@ -37,5 +59,8 @@ class SearchResult {
         this.onClick(this.data[index]);
       });
     });
+
+    let listItems = this.$searchResult.querySelectorAll('.item');
+    this.applyEventToElement(listItems);
   }
 }
